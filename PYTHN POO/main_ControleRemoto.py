4dae6e6e2f77e6297aabb44c1c8a50f17bdb9204
Carolina Mantovane <carol_mantovane@hotmail.com>
2025-09-16 12:07:35 -0300

    Comandos de classes
    Exemplo Controle Remoto

---
+++ PYTHN POO/main_ControleRemoto.py
@@ -0,0 +1,30 @@
+class ControleRemoto:
+    def __init__(self, cor, altura, profundidade, largura):
+        self.cor = cor
+        self.altura = altura
+        self.profundidade = profundidade
+        self.largura = largura
+
+    def mudar_volume(self, botao):
+        if botao == "+":
+            print("Aumentar o volume")
+        elif botao == "-":
+            print("Diminuir o volume!")   
+
+
+controle1 = ControleRemoto("branca", "10cm", "2cm", "3cm")
+controle1.mudar_volume("+")
+controle2 = ControleRemoto("preto", "15cm", "5cm", "5cm")
+print(f"\nA cor do controle 1 é {controle1.cor} e a sua altura é de {controle1.altura}")
+
+# Características do controle:
+# - Cor
+# - Altura
+# - Profundidade
+# - Largura
+
+# #Métodos (funções que exerce):
+# - Power
+# - Mexernovolume
+# - Mute
+# - Menu
